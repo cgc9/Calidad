8082b8a1772ba9072551a6b6e946e751e0b7d87a
pau0911 <pau96go@gmail.com>
2020-04-22 20:43:45 -0500

    Validaciones y diseno

--- figuras.js
+++ figuras.js
@@ -1,4 +1,7 @@
+
+
 function triangulo() {
+
     var ladoUno = document.getElementById('ladoUno').value;
     console.log(ladoUno);
     var ladoDos = document.getElementById('ladoDos').value;
@@ -6,37 +9,56 @@ function triangulo() {
     var ladoTres = document.getElementById('ladoTres').value;
     console.log(ladoTres);
 
+    if (ladoDos && ladoUno && ladoTres) {
 
-    if (ladoUno == ladoDos && ladoDos == ladoTres) {
-        alert("equilatero");
-    } else if (ladoUno != ladoDos && ladoUno != ladoTres && ladoDos != ladoTres) {
-        alert("Escaleno");
+        if (ladoDos > 0 && ladoUno > 0 && ladoTres > 0) {
 
-    } else
-        alert("Isoceles");
+            if (ladoUno == ladoDos && ladoDos == ladoTres) {
+                alert("Equilatero");
+            } else if (ladoUno != ladoDos && ladoUno != ladoTres && ladoDos != ladoTres) {
+                alert("Escaleno");
+            } else
+                alert("Isoceles");
+        }else{
+            alert("Los valores de los lados deben ser diferentes y mayores que 0")
+        }
+    } else {
+        alert("Ingrese por favor todos los campos para el triángulo")
+    }
+
+
+
 }
 
-function calcularFigura(){
+function calcularFigura() {
     var ladoUno = document.getElementById('lado1').value;
     var ladoDos = document.getElementById('lado2').value;
     var ladoTres = document.getElementById('lado3').value;
     var ladoCuatro = document.getElementById('lado4').value;
 
-    var lados=[ladoUno,ladoDos,ladoTres,ladoCuatro]
+    if (ladoUno && ladoDos && ladoTres && ladoCuatro) {
 
-    var uniqs = lados.filter(function(item, index, array) {
-        return array.indexOf(item) === index;
-      })
-      console.log(uniqs); 
+        if(ladoDos > 0 && ladoUno > 0 && ladoTres > 0 && ladoCuatro >0){
+            var lados = [ladoUno, ladoDos, ladoTres, ladoCuatro]
+            var uniqs = lados.filter(function (item, index, array) {
+                return array.indexOf(item) === index;
+            })
+            console.log(uniqs);
+    
+            if (uniqs.length == 1) {
+                alert("Cuadrado");
+    
+            } else if (uniqs.length == 2) {
+                alert("Rectangulo");
+    
+            } else if (uniqs.length == 3)
+                alert("Trapecio");
+        }else{
+            alert("Los valores de los lados deben ser diferentes y mayores que 0");
+        }
+    }else{
+        alert("Ingrese por favor todos los campos para el cuadrilátero");
+    }
 
-      if(uniqs.length==1){
-          alert("Cuadrado");
-
-      }else if(uniqs.length==2){
-          alert("Rectangulo");
-
-      }else if(uniqs.length==3)
-        alert("Trapecio");
-      
 
 }
